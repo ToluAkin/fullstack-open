8a128f393aa70c654d63fc66002c781783d41d1d
Toluwalase <tolu@cottacush.com>
2021-05-26 23:53:04 +0100

    2.12 done

--- part2/countriesdata/src/App.js
+++ part2/countriesdata/src/App.js
@@ -1,12 +1,11 @@
 import './App.css'
-import React , {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
 	const [ country, setCountry ] = useState('')
-	const [countries, setCountries] = useState([])
-	
-	let copyCountry = country
-	let copyCountries = countries
+	const [ countries, setCountries ] = useState([])
+	const [ matched, setMatched ] = useState([])
 
     useEffect(() => {
         axios.get('https://restcountries.eu/rest/v2/all')
@@ -16,45 +15,41 @@ const App = () => {
     }, [])
 
 	const handleInput = (e) => {
-        setCountry(e.target.value)
-        let matchedCountries = []
-        copyCountries.forEach(item => {
-            let matched = item.name.toLowerCase().includes(country)
-            if (matched) {
-                matchedCountries.push(item)
-                if (matchedCountries.length === 1) {
-                    let oneCountry = matchedCountries[0]
-                    let languages = oneCountry.languages.map(language => {
-                        return <li>{ language.name }</li>
-                    })
-                    return (
-                        <div>
-                            <h2>{oneCountry.name}</h2>
-                            <p>Capital {oneCountry.capital}</p>
-                            <p>Population {oneCountry.population}</p>
-                            <h3>Languages</h3>
-                            <ul>{ languages }</ul>
-                            <img src={ oneCountry.flag } alt={oneCountry.name}></img>
-                        </div>
-                    )
-                } else if (matchedCountries.length > 0 && matchedCountries.length <= 10) {
-                    return (
-                        matchedCountries.map(item => {
-                            return <p key={item.numericCode}>{item.name}</p>
-                        })
-                    )
-                } else if (matchedCountries.length > 10) {
-                    return (
-                        <p>Too many matches, specify another filter</p>
-                    )
-                }
-            }
-        })
-    }
+		setCountry(e.target.value);
+		let result = countries.filter(singleCountry => {
+			return singleCountry.name.toLowerCase().includes(country)
+		});
+		setMatched(result);
+	}
+	
+	let matchedCountries;
+	if (matched.length === 1) {
+		let oneCountry = matched[0]
+		let languages = oneCountry.languages.map((language, index) => {
+			return <li key={ index }>{ language.name }</li>
+		})
+		matchedCountries =
+			<div>
+				<h2>{ oneCountry.name }</h2>
+				<p>Capital { oneCountry.capital }</p>
+				<p>Population { oneCountry.population }</p>
+				<h3>Languages</h3>
+				<ul>{ languages }</ul>
+			<img src={oneCountry.flag} alt={oneCountry.name} style={{ height: '200px' }}></img>
+			</div>
+	} else if (matched.length > 0 && matched.length <= 10) {
+		matchedCountries = matched.map(item => {
+			return <p key={item.numericCode}>{item.name}</p>
+		})
+	} else if (matched.length > 10) {
+		matchedCountries = <p>Too many matches, specify another filter</p>
+	}
 
 	return (
-		<div> find countries
-            <input value={copyCountry} onInput={handleInput} />
+		<div> 
+			<span>find countries</span>
+			<input value={country} onInput={handleInput} />
+			{ matchedCountries }
         </div>
 	)
 }
