a9dbed3efe2b8e8f0988594074de756ea5d4f3a9
Toluwalase <tolu@cottacush.com>
2021-06-20 23:00:11 +0100

    Exercise 4.1-4.5 done

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -1,7 +1,6 @@
 const listHelper = require('../utils/list_helper')
 
-describe('total likes', () => {
-  const blogs = [
+const blogs = [
   {
     _id: "5a422a851b54a676234d17f7",
     title: "React patterns",
@@ -52,8 +51,30 @@ describe('total likes', () => {
   }  
 ]
 
+describe('total likes', () => {
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(36)
   })
+})
+
+describe('highest likes', () => {
+  test('blog with highest likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    })
+  })
+})
+
+describe('most blogs', () => {
+    test('author with most blogs', () => {
+        const result = listHelper.mostBlogs(blogs)
+        expect(result).toEqual({
+            author: "Robert C. Martin",
+            blogs: 3
+        })
+    })
 })
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,7 +1,20 @@
-const totalLikes = (blogs) => {
+const totalLikes = blogs => {
     return blogs.map(blog => blog.likes).reduce((a,b) => { return a + b })
 }
 
+const favoriteBlog = blogs => {
+    let maxValue = Math.max(...blogs.map(blog => blog.likes))
+    let match = blogs.filter(blog => blog.likes == maxValue)
+
+    return {
+        "title": match[0].title,
+        "author": match[0].author,
+        "likes": match[0].likes
+    }
+}
+
+const mostBlogs = blogs => { }
+
 module.exports = {
-    totalLikes
+    totalLikes, favoriteBlog, mostBlogs
 }
