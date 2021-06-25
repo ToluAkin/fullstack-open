43df08d153ae716ba9ca44e14407923b481146b7
Toluwalase <tolu@cottacush.com>
2021-06-25 07:52:00 +0100

    Exercises 4.13.-4.14 done with tests

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -5,22 +5,33 @@ const router = express.Router()
 const Blog = require('../models/blog')
 
 //get request for all blogs
-router.get('/', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => { res.json(blogs) })
+router.get('/', async(req, res) => {
+    const blogs = await Blog.find({})
+    res.json(blogs)
 })
 
 //post request to create a blog
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
     if (!req.body.title && !req.body.url) {
         res.status(400).end()
     } else {
         const blog = new Blog(req.body)
-        blog.save()
-            .then(result => { res.status(201).json(result) })
+        const savedBlog = await blog.save()
+        res.status(201).json(savedBlog)
     }
-  
 })
 
+// Remove a blog
+router.delete('/:id', async (req, res) => {
+    const id = await req.params.id
+    await Blog.findByIdAndRemove(id)
+    res.status(204).end()
+})
+
+// updating a blog
+router.put('/:id', async (req, res) => {
+    const id = await req.params.id
+    const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, { new: true })
+    res.json(updatedBlog)
+})
 module.exports = router
--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -11,63 +11,108 @@ beforeEach(async () => {
     await Blog.insertMany(blogs)
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('viewing blogs saved', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('is unique identifier defined', async () => {
+        const response = await api.get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        
+        const resBody = response.body.map(blog => blog.id)
+        expect(resBody).toBeDefined()
+    })
 })
 
-test('is unique identifier defined', async () => {
-    const response = await api.get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    const resBody = response.body.map(blog => blog.id)
-    expect(resBody).toBeDefined()
+
+describe('addition of blog details', () => {
+    test('sending post data', async () => {
+        let allBlogs = await Blog.find({})
+        
+        const newBlog = {
+            title: "Twitter Days",
+            author: "Jack",
+            url: "https://twitter.com",
+            likes: 10000000000
+        }
+
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        const newBlogs = await Blog.find({})
+        expect(newBlogs).toHaveLength(allBlogs.length + 1)
+        expect(newBlogs[allBlogs.length].title).toContain(newBlog.title)
+    })
+
+    test('likes property to zero', async () => {
+        const newBlog = {
+            title: "Taekwando Days",
+            author: "Jackie Chan",
+            url: "https://twitter.com/jackie-chan",
+        }
+
+        newBlog.likes === undefined ? newBlog.likes = 0 : newBlog.likes
+        
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        expect(newBlog.likes).toBe(0)
+    })
+
+    test('incomplete blog object', async () => {
+        const newBlog = {
+            author: "Chains",
+            likes: 10000000000
+        }
+
+        const response = await api.post('/api/blogs').send(newBlog)
+        
+        expect(response.statusCode).toBe(400)
+    })
 })
 
-test('sending post data', async () => {
-    let allBlogs = await Blog.find({})
-    let existingLength = allBlogs.length
-    let expectedLength = existingLength + 1
-    const newBlog = {
-        title: "Twitter Days",
-        author: "Jack",
-        url: "https://twitter.com",
-        likes: 10000000000
-    }
+describe('updating a specific blog', () => {
+    test('deleting a blog', async () => {
+        const allBlogs = await Blog.find({})
+        const newBlog = {
+            title: "Breakup songs",
+            author: "Taylor Swift",
+            url: "https://taylorswift.me",
+            likes: 10000000000
+        }
 
-    await api.post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-    const newBlogs = await Blog.find({})
-    expect(newBlogs).toHaveLength(expectedLength)
-    expect(newBlogs[existingLength].title).toBe(newBlog.title)
-})
+        const response = await api.post('/api/blogs').send(newBlog)
+        const deletedBlog = await api.delete(`/api/blogs/${response.body.id}`)
+        
+        expect(deletedBlog.statusCode).toBe(204)
+        expect(allBlogs).not.toContain(newBlog)
+    })
 
-test('likes property to zero', async () => {
-    const newBlog = {
-        title: "Taekwando Days",
-        author: "Jackie Chan",
-        url: "https://twitter.com/jackie-chan",
-    }
+    test('updating a blog', async () => {
+        const allBlogs = await Blog.find({})
+        const blogToChange = allBlogs[0]
+        const blogUpdate = {
+            title: "Breakup songs",
+            author: "Taylor Swift",
+            url: "https://taylorswift.me",
+        }
 
-    newBlog.likes === undefined ? newBlog.likes = 0 : newBlog.likes
-    await api.post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-    expect(newBlog.likes).toBe(0)
-})
-
-test('incomplete blog object', async () => {
-    const newBlog = {
-        author: "Chains",
-        likes: 10000000000
-    }
-    const response = await api.post('/api/blogs').send(newBlog)
-    expect(response.statusCode).toBe(400)
+        const response = await api.put(`/api/blogs/${blogToChange.id}`)
+            .send(blogUpdate)
+            .expect('Content-Type', /application\/json/)
+        expect(response.body.title).toContain(blogUpdate.title)
+        expect(response.body.author).toContain(blogUpdate.author)
+        expect(response.body.url).toContain(blogUpdate.url)
+    })
 })
 
 afterAll(() => {
