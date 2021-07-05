2c0460537830fc04024491dd027f269dd24f35eb
Toluwalase <tolu@cottacush.com>
2021-07-05 20:00:34 +0100

    exercise 4.6 & 4.7 done

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -1,30 +1,31 @@
 const listHelper = require('../utils/list_helper')
 const blogs = require('../utils/blog_list_helper').blogs
 
-describe('total likes', () => {
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(36)
-  })
-})
-
-describe('highest likes', () => {
-  test('blog with highest likes', () => {
-    const result = listHelper.favoriteBlog(blogs)
-    expect(result).toEqual({
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      likes: 12,
+describe('blogs detail on likes', () => {
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(36)
     })
-  })
+
+    test('blog with highest likes', () => {
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toEqual({
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12,
+        })
+    })
+
+    test('blog with with mot likes', () => {
+        const result = listHelper.mostLikes(blogs)
+        expect(result).toEqual({ author: "Edsger W. Dijkstra", likes: 17 })
+    })
+
 })
 
-// describe('most blogs', () => {
-//     test('author with most blogs', () => {
-//         const result = listHelper.mostBlogs(blogs)
-//         expect(result).toEqual({
-//             author: "Robert C. Martin",
-//             blogs: 3
-//         })
-//     })
-// })
+describe('most blogs', () => {
+    test('author with most blogs', () => {
+        const result = listHelper.mostBlogs(blogs)
+        expect(result).toEqual({ author: "Robert C. Martin", blogs: 3 })
+    })
+})
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,3 +1,4 @@
+
 const totalLikes = blogs => {
     return blogs.map(blog => blog.likes).reduce((a,b) => { return a + b })
 }
@@ -13,8 +14,23 @@ const favoriteBlog = blogs => {
     }
 }
 
-const mostBlogs = blogs => { }
-
-module.exports = {
-    totalLikes, favoriteBlog, mostBlogs
+const mostBlogs = blogs => {
+    // const maxAuthor = authorNames.reduce((prev, cur) => (prev.y > cur.y) ? prev : cur)
+    const keysAndValues = blogs.reduce((prev, cur) => {
+        prev[cur.author] = (prev[cur.author] || 0) + 1;
+        return prev;
+    }, {});
+    const maxAuthor = Object.entries(keysAndValues).reduce((prev, cur) => (prev[1] > cur[1]) ? prev : cur)
+    return { author: maxAuthor[0], blogs: maxAuthor[1] }
 }
+
+const mostLikes = blogs => {
+    const keysAndValues = blogs.reduce((prev, cur) => {
+        prev[cur.author] = (prev[cur.author] || 0) + cur.likes;
+        return prev;
+    }, {});
+    const maxAuthorData = Object.entries(keysAndValues).reduce((prev, cur) => (prev[1] > cur[1]) ? prev : cur)
+    return { author: maxAuthorData[0], likes: maxAuthorData[1] }
+}
+
+module.exports = { totalLikes, favoriteBlog, mostBlogs, mostLikes }
